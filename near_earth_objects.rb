65688d2971bc8f70870519d30d9b6245f397a79f
Kevin McGrevey <niv3kmcg@gmail.com>
2020-04-27 14:36:16 -0600

    Clears commented code

--- near_earth_objects.rb
+++ near_earth_objects.rb
@@ -13,21 +13,7 @@ class NearEarthObjects
     )
     asteroids_list_data = conn.get('/neo/rest/v1/feed')
 
-    # parsed_asteroids_data = JSON.parse(asteroids_list_data.body, symbolize_names: true)[:near_earth_objects][:"#{date}"]
     @parsed_asteroids_data = JSON.parse(asteroids_list_data.body, symbolize_names: true)[:near_earth_objects][:"#{date}"]
-
-    # largest_astroid_diameter = parsed_asteroids_data.map do |astroid|
-    #   astroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i
-    # end.max { |a,b| a<=> b}
-
-    # total_number_of_astroids = @parsed_asteroids_data.count
-    # formatted_asteroid_data = @parsed_asteroids_data.map do |astroid|
-    #   {
-    #     name: astroid[:name],
-    #     diameter: "#{astroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i} ft",
-    #     miss_distance: "#{astroid[:close_approach_data][0][:miss_distance][:miles].to_i} miles"
-    #   }
-    # end
 
     {
       astroid_list: formatted_asteroid_data,
