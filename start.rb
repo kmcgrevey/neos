67c19d0351dc7fe5c7c791b0b5f85100189bd563
Kevin McGrevey <niv3kmcg@gmail.com>
2020-04-27 19:30:10 -0600

    Cleans out commented code

--- start.rb
+++ start.rb
@@ -1,10 +1,5 @@
 require_relative 'near_earth_objects'
 
-# puts "________________________________________________________________________________________________________________________________"
-# puts "Welcome to NEO. Here you will find information about how many meteors, astroids, comets pass by the earth every day. \nEnter a date below to get a list of the objects that have passed by the earth on that day."
-# puts "Please enter a date in the following format YYYY-MM-DD."
-# print ">>"
-
 def start
   intro
   @date = gets.chomp
@@ -12,23 +7,11 @@ def start
   results
 end
 
-# astroid_list = astroid_details[:astroid_list]
-# total_number_of_astroids = astroid_details[:total_number_of_astroids]
-# largest_astroid = astroid_details[:biggest_astroid]
-
-# column_labels = { name: "Name", diameter: "Diameter", miss_distance: "Missed The Earth By:" }
-# column_data = column_labels.each_with_object({}) do |(col, label), hash|
-#   hash[col] = {
-  #     label: label,
-  #     # width: [astroid_list.map { |astroid| astroid[col].size }.max, label.size].max}
-  #     width: [astroid_details[:astroid_list].map { |astroid| astroid[col].size }.max, label.size].max}
-  # end
 def column_labels
   { name: "Name", diameter: "Diameter", miss_distance: "Missed The Earth By:" }
 end
 
 def column_data
-  # column_labels = { name: "Name", diameter: "Diameter", miss_distance: "Missed The Earth By:" }
   column_labels.each_with_object({}) do |(col, label), hash|
     hash[col] = {
       label: label,
@@ -36,8 +19,6 @@ def column_data
   end
 end
 
-# header = "| #{ column_data.map { |_,col| col[:label].ljust(col[:width]) }.join(' | ') } |"
-# divider = "+-#{column_data.map { |_,col| "-"*col[:width] }.join('-+-') }-+"
 def header
   "| #{ column_data.map { |_,col| col[:label].ljust(col[:width]) }.join(' | ') } |"
 end
@@ -58,20 +39,12 @@ end
 def formatted_date
   DateTime.parse(@date).strftime("%A %b %d, %Y")
 end
-# formated_date = DateTime.parse(date).strftime("%A %b %d, %Y")
-# puts "______________________________________________________________________________"
-# puts "On #{formatted_date}, there were #{@astroid_details[:total_number_of_astroids]} objects that almost collided with the earth."
-# puts "The largest of these was #{@astroid_details[:biggest_astroid]} ft. in diameter."
-# puts "\nHere is a list of objects with details:"
-# puts divider
-# puts header
-# create_rows(@astroid_details[:astroid_list], column_data)
-# puts divider
+
 def intro
   puts "________________________________________________________________________________________________________________________________"
   puts "Welcome to NEO. Here you will find information about how many meteors, astroids, comets pass by the earth every day. \nEnter a date below to get a list of the objects that have passed by the earth on that day."
   puts "Please enter a date in the following format YYYY-MM-DD."
-  print ">>"
+  print ">> "
 end
 
 def results
